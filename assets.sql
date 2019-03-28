74e32d29206615a47c39a5b4b7cb3bd955c301d9
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-03-28 13:49:57 +0100

    Update assets.sql

--- assets.sql
+++ assets.sql
@@ -45,16 +45,6 @@ GO
 ------------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------------
-CREATE TABLE dbo.Models(
-  ModelID     INT IDENTITY(1,1),
-  ModelName   NVARCHAR(20),
-  CONSTRAINT  PK_Models
-  PRIMARY KEY (ModelID)
-);
-GO
-------------------------------------------------------------------------------------
-
-------------------------------------------------------------------------------------
 CREATE TABLE dbo.Computers (
   ComputerID    INT IDENTITY(1,1),
   AssetTag      NVARCHAR(15)  NULL,
@@ -62,16 +52,13 @@ CREATE TABLE dbo.Computers (
   MACAdress     NVARCHAR(50)  NOT NULL,
   BIOSGUID      NVARCHAR(50)  NULL,
   SCCMDate      DATETIME      NULL,
+  ModelName     NVARCHAR(50)  NOT NULL,
   ZoneID        INT           NOT NULL,
-  ModelID       INT           NOT NULL,
   CONSTRAINT    PK_Computers
   PRIMARY KEY   (ComputerID),
   CONSTRAINT    FK_Zones_ZoneID
   FOREIGN KEY   (ZoneID)
-  REFERENCES    dbo.Zones,
-  CONSTRAINT    FK_Models_ModelID
-  FOREIGN KEY   (ModelID)
-  REFERENCES    dbo.Models
+  REFERENCES    dbo.Zones
 );
 GO
 ------------------------------------------------------------------------------------
@@ -84,13 +71,11 @@ SELECT  C.ComputerID,
         C.Serialnumber,
         C.MACAdress,
         C.SCCMDate,
-        Z.ZoneName,
-        M.ModelName
+        C.ModelName,
+        Z.ZoneName
 FROM    dbo.Computers AS C
 JOIN    dbo.Zones AS Z
         ON C.ZoneID = Z.ZoneID
-JOIN    dbo.Models AS M
-        ON C.ModelID = M.ModelID;
 GO
 ------------------------------------------------------------------------------------
 
@@ -101,17 +86,16 @@ CREATE PROCEDURE dbo.InsertComputer(
   @MACAdress    NVARCHAR(50),
   @SCCMDate     DATETIME = NULL,
   @ZoneID       INT = 1, -- ZoneID 1 -eq "unknown"
-  @ModelName    NVARCHAR(20)
+  @ModelName    NVARCHAR(50)
 )
 AS
-DECLARE @ModelID INT = (SELECT TOP 1 ModelID FROM dbo.Models WHERE ModelName = @ModelName);
 INSERT INTO dbo.Computers(
   AssetTag,
   Serialnumber,
   MACAdress,
   SCCMDate,
   ZoneID,
-  ModelID
+  ModelName
 )
 VALUES (
   @AssetTag,
@@ -119,17 +103,7 @@ VALUES (
   @MACAdress,
   @SCCMDate,
   @ZoneID,
-  @ModelID
+  @ModelName
 );
-GO
-------------------------------------------------------------------------------------
-
-------------------------------------------------------------------------------------
-CREATE PROCEDURE dbo.InsertModel(
-  @ModelName  NVARCHAR(20)
-)
-AS
-INSERT INTO dbo.Models(ModelName)
-VALUES      (@ModelName);
 GO
 ------------------------------------------------------------------------------------
